2fc7abe83af87418c180824a3647442e71b8dc35
TheMiranha <lxbrmiranda@gmail.com>
2025-11-29 02:13:45 -0300

    feat: i18n setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-export default function Home() {
-  return (
-   <div>
-    Hello World!
-   </div>
-  );
+import {getTranslations} from 'next-intl/server';
+ 
+export default async function HomePage() {
+  const t = await getTranslations('Home');
+  return <h1>{t('Text1')}</h1>;
 }
